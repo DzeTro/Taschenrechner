139cf5b5d86d18b9583e8f06e6dd848fcf008032
Dzevad Trokic <d.trokic@cp-pro.de>
2021-12-23 14:32:07 +0100

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Taschenrechner");
+            int number1 = int.Parse(Console.ReadLine());
+            string zeichen = new string(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            
+            if (zeichen == "+")
+            {
+                int result = number1 + number2;
+                Console.WriteLine(result);
+            }
+            else if (zeichen == "-")
+            {
+                int result = number1 - number2;
+                Console.WriteLine(result);
+            }
+            else if (zeichen == "*")
+            {
+                int result = number1 * number1;
+                Console.WriteLine(result);
+            }
+            else if (zeichen == "/")
+            { 
+                int result = number1 / number2;
+                Console.WriteLine(result);
+            }
+            
+        }
+    }
+}
